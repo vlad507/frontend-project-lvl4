84f3c58689ccf3f21dec706afac7b859b9958092
vlad <222vladik@gmail.com>
2020-07-12 17:41:40 +0300

    make 1st step

--- src/index.js
+++ src/index.js
@@ -10,9 +10,19 @@ import gon from 'gon';
 // import cookies from 'js-cookie';
 // import io from 'socket.io-client';
 
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import ChannelList from './ChannelList.jsx';
+
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
 console.log('it works!');
 console.log('gon', gon);
+
+ReactDOM.render(
+  <ChannelList channels={gon.channels}/>,
+  document.querySelector('.container'),
+);
